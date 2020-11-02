94e8f1c76da4c8fc3937c0f5b109c1660dd082db
zyhcool <13105027620@163.com>
2020-11-02 10:27:17 +0800

    poll

--- src/utils/pollManager.ts
+++ src/utils/pollManager.ts
@@ -3,27 +3,45 @@
 export default class PollManager {
 
     static async runTask(count: number, timeout: number, fn: Function, ...args) {
+        console.log('count: ', count)
         return new Promise((resolve, reject) => {
             if (count === 0) {
+                reject(new Error('task failed'))
                 return
             }
             setTimeout(() => {
-                fn(...args).then((err, ...res) => {
+                const errorHandle = (err) => {
+                    if (/is not ready/.test(err.message)) {
+                        this.runTask(--count, timeout, fn, ...args).then(resultHandle)
+                    } else {
+                        reject(err)
+                    }
+                }
+                const resultHandle = (err, ...res) => {
                     if (!err) {
                         resolve(res)
-                    } else {
-                        if (/is not ready/.test(err.message)) {
-                            this.runTask(--count, timeout, fn, ...args).then(v => resolve(v))
-                        } else {
-                            reject(err)
-                        }
                     }
-                })
+                    else {
+                        errorHandle(err)
+                    }
+                }
+                fn(...args)
+                    .then(resultHandle)
+                    .catch(errorHandle)
             }, timeout * 1000);
-
         })
     }
+
+
 }
 
 
 
+async function test() {
+    throw new Error('is not ready');
+}
+
+(async () => {
+    let res = await PollManager.runTask(2, 3, test)
+    console.log(res)
+})()
