2cb83ad8bd423fb19dc2f05a8f4d005d2ece823a
zyhcool <13105027620@163.com>
2020-11-18 18:44:34 +0800

    s

--- src/workers/createVM.ts
+++ src/workers/createVM.ts
@@ -1,23 +1,23 @@
 
-import { parentPort } from "worker_threads";
-import GcpManager from "../cloud/gcpManager";
-import gcpManager from '../cloud/gcpManager';
+// import { parentPort } from "worker_threads";
+// import GcpManager from "../cloud/gcpManager";
+// import gcpManager from '../cloud/gcpManager';
 
 
-parentPort.on('message', async (data) => {
-    if (data.cmd === 'start') {
-        try {
-            const { orderId, time, config } = data.args
-            const gcp = new GcpManager(orderId, time, 1, config)
-            const res = gcp.start();
-            parentPort.postMessage({ event: 'done', result: res });
+// parentPort.on('message', async (data) => {
+//     if (data.cmd === 'start') {
+//         try {
+//             const { orderId, time, config } = data.args
+//             const gcp = new GcpManager(orderId, time, 1, config)
+//             const res = gcp.start();
+//             parentPort.postMessage({ event: 'done', result: res });
 
-        }
-        catch (e) {
-            parentPort.postMessage({ event: 'error', error: e })
-        }
-    }
-})
+//         }
+//         catch (e) {
+//             parentPort.postMessage({ event: 'error', error: e })
+//         }
+//     }
+// })
 
 
 
