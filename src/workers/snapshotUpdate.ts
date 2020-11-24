27e2434f10f71772493992ac443fb4af71bf6668
zyhcool <13105027620@163.com>
2020-11-24 17:53:19 +0800

    hiahia

--- src/workers/snapshotUpdate.ts
+++ src/workers/snapshotUpdate.ts
@@ -1,21 +1,21 @@
 
-import { parentPort } from "worker_threads";
-import Gcp from "../cloud/gcpManager";
-import gcpManager from '../cloud/gcpManager';
+// import { parentPort } from "worker_threads";
+// import Gcp from "../cloud/gcpManager";
+// import gcpManager from '../cloud/gcpManager';
 
 
-parentPort.on('message', async (data) => {
-    if (data.cmd === 'start') {
-        try {
-            const res = await Gcp.updateSnapshot();
-            parentPort.postMessage({ event: 'done', result: res });
+// parentPort.on('message', async (data) => {
+//     if (data.cmd === 'start') {
+//         try {
+//             const res = await Gcp.updateSnapshot();
+//             parentPort.postMessage({ event: 'done', result: res });
 
-        }
-        catch (e) {
-            parentPort.postMessage({ event: 'error', error: e })
-        }
-    }
-})
+//         }
+//         catch (e) {
+//             parentPort.postMessage({ event: 'error', error: e })
+//         }
+//     }
+// })
 
 
 
